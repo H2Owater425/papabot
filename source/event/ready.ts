feabbf47f6e05f1e86cda10cd0b8f847d51f8d7b
H2Owater425 <h2o@h2owr.xyz>
2022-08-06 04:50:43 +0900

    fix: Fix status message

--- source/event/ready.ts
+++ source/event/ready.ts
@@ -5,7 +5,7 @@ import { client } from '@application';
 export default new Event('ready', function (): void {
 	client.editStatus('online', {
 		type: 3,
-		name: process['env']['PREFIX'] + '!도움'
+		name: process['env']['PREFIX'] + '도움'
 	});
 
 	logger.info('ready as ' + client['user']['username'] + '(' + client['user']['id'] + ')'); // TODO: Change ready message more informative
